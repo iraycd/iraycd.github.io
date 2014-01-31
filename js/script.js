250359dfc1cf053f1f12f5e32579d839b2482673
Ray Ch <i@iraycd.com>
2014-02-01 00:45:05 +0530

    New version with angularjs and images

---
+++ js/script.js
@@ -0,0 +1,41 @@
+var project = angular.module('project', ['ngRoute']);
+
+
+project.config(function($routeProvider) {
+    $routeProvider
+     .when('/', {
+        controller:'HomeCtrl',
+        templateUrl:'home.html'
+    })
+    .when('/skills', {
+        controller:'SkillCtrl',
+        templateUrl:'skills.html'
+    })
+    .when('/work', {
+        controller:'WorkCtrl',
+        templateUrl:'work.html'
+    })
+    .when('/blog', {
+        controller:'BlogCtrl',
+        templateUrl:'blog.html'
+    })
+    .otherwise({
+        redirectTo:'/'
+    });
+});
+
+
+project.controller('HomeCtrl', function($scope) {
+});
+
+project.controller('SkillCtrl', function($scope) {
+     $scope.skill = "active";
+});
+ 
+project.controller('WorkCtrl', function($scope) {
+     $scope.work = "active";
+});
+ 
+project.controller('BlogCtrl', function($scope) {
+     $scope.blog = "active";
+});
